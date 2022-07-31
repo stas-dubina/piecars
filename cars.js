f5ef4f0eb86088318897b24849033a543ad31019
gennadiy-dubina <gennadiy.dubina@dataart.com>
2022-07-31 15:54:42 +0300

    add cars list

---
+++ cars.js
@@ -0,0 +1,38 @@
+const COUNTRY_GERMANY = 'germany'
+const COUNTRY_ITALY = 'italy'
+const COUNTRY_USA = 'usa'
+
+const cars = [
+    {
+        title: "Alfa Romeo Giulia",
+        img: "alfa-romeo-giulia.jpg",
+        year: 2016,
+        fuel: "Gas, 2.0",
+        hp: 240,
+        country: COUNTRY_ITALY
+    },
+    {
+        title: "BMW i3",
+        img: "i3.jpg",
+        year: 2015,
+        fuel: "Hybrid, 0.65",
+        hp: 170,
+        country: COUNTRY_GERMANY
+    },
+    {
+        title: "Porsche Cayenne",
+        img: "cayenne.jpg",
+        year: 2017,
+        fuel: "Gas, 3.0",
+        hp: 340,
+        country: COUNTRY_GERMANY
+    },
+    {
+        title: "Tesla Model S",
+        img: "tesla.jpg",
+        year: 2017,
+        fuel: "Electro",
+        hp: 362,
+        country: COUNTRY_USA
+    }
+]
